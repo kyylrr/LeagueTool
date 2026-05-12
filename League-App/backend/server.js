b68f4bcd1d9d90813b7f34a39cf95951ca257bd4
kyler wang <kylerwang5@gmail.com>
2026-05-12 00:09:50 -0700

    learned about parameters in express, routes, better understanding of flow, bettern understand ing of api, json, express and axios. still need to work on express and axios api communication

--- League-App/backend/server.js
+++ League-App/backend/server.js
@@ -5,8 +5,13 @@ const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("backend is working");
+app.get("/summoner/:gameName/:tagline", async (req, res) => {
+    const userId =req.params.gameName;
+    const tag =req.params.tagline;
+
+    const region = 'na1';
+    axios.get("https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/userId/tagline")
+
 });
 app.listen(3000, () => {
     console.log("server running on port 3000");
